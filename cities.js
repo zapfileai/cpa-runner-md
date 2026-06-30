ed0952e35ae0293279b977ad65499055f26a7cae
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:19:11 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
-﻿const marylandCities = [
-  // paste first 80 cities from cities.csv here
+const marylandCities = [
+  "Aberdeen", "Abingdon", "Accident", "Accokeek", "Adamstown",
+  "Andrews", "Annapolis", "Annapolis Junction", "Arnold", "Ashton",
+  "Baldwin", "Baltimore", "Barnesville", "Barton", "Bel Air",
+  "Bel Alton", "Belcamp", "Beltsville", "Berlin", "Bethesda",
+  "Bishopville", "Bladensburg", "Boonsboro", "Bowie", "Boyds",
+  "Bozman", "Braddock Heights", "Brandywine", "Brinklow", "Brookeville",
+  "Brooklandville", "Brooklyn", "Broomes Island", "Brownsville", "Brunswick",
+  "Bryans Road", "Bryantown", "Buckeystown", "Burtonsville", "Cabin John",
+  "California", "Callaway", "Cambridge", "Capitol Heights", "Catonsville",
+  "Cecilton", "Centreville", "Chaptico", "Charlestown", "Charlotte Hall",
+  "Cheltenham", "Chesapeake Beach", "Chesapeake City", "Chester", "Chestertown",
+  "Chevy Chase", "Childs", "Church Creek", "Church Hill", "Churchton",
+  "Churchville", "Clarksburg", "Clarksville", "Clear Spring", "Clinton",
+  "Cockeysville", "College Park", "Colora", "Columbia", "Conowingo",
+  "Cooksville", "Cordova", "Crisfield", "Crofton", "Crownsville",
+  "Cumberland", "Curtis Bay", "Damascus", "Darlington", "Davidsonville",
 ];
 
 const BATCH_SIZE = 5;
